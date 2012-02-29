707504de9e5eaaefc0e2252a328b232d47923287
Frank Joseph Greer <frankj@perquisitoromnia.net>
2012-02-29 10:48:40 -0600

    frontend

--- frontend.c
+++ frontend.c
@@ -17,7 +17,28 @@
  */
 
 #include <stdio.h>
+#include <termios.h>
 #include "frontend.h"
+
+char getch(void) {
+  char buf = 0;
+  struct termios old = {0};
+  if (tcgetattr(0, &old) < 0)
+    perror("tcsetattr()");
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+  if (tcsetattr(0, TCSANOW, &old) < 0)
+    perror("tcsetattr ICANON");
+  if (read(0, &buf, 1) < 0)
+    perror ("read()");
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+  if (tcsetattr(0, TCSADRAIN, &old) < 0)
+    perror ("tcsetattr ~ICANON");
+  return (buf);
+}
 
 int show_menu(menu_t* menu,int num,int deflt) {
   int retval = 0;
